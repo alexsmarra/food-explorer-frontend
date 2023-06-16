a465938dcdfeb338a97cc1b64c7fe5099ceb7765
Alexandre Marra <alexsmarra@hotmail.com>
2023-06-16 17:04:14 -0300

    test

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,10 +1,33 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useState } from "react"
+
+import { api } from "../services/api.js"
 
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+   const [data, setData] = useState({})
+
+   /* params entre chaves, assim, sempre que utilizarmos a nossa function 'signIn' não precisaremos
+   chamar os params necessariamente nessa ordem */
+   async function signIn({ email, password }) {
+      try {
+         /* Irá pegar o email e senha digitado pelo usuário nos inputs da page SignIn e enviará
+         para o nosso backend (para o nosso sessionController.js analisar) */
+         const response = await api.post("/sessions", { email, password })
+         console.log(response)
+
+      } catch(error) {
+         if(error.response) {
+            /* chama a msg do AppError de nosso backend */
+            alert(error.response.data.message)
+         } else {
+            alert("Não foi possível entrar!")
+         }
+      }
+   }
+
    return (
-      <AuthContext.Provider value={{ name: 'Alex', email: 'alex@gmail.com' }}>
+      <AuthContext.Provider value={{ signIn }}>
          {children}
       </AuthContext.Provider>
    )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,7 @@
-import { Container, Head, Form } from './styles'
-
-// para exemplo:
+import { useState } from 'react'
 import { useAuth } from '../../hooks/auth'
+
+import { Container, Head, Form } from './styles'
 
 import { useNavigate } from 'react-router-dom'
 
@@ -12,11 +12,18 @@ import { Button } from '../../components/Button'
 import LogoPolygon from '../../assets/menu/Polygon.svg'
 
 export function SignIn() {
+   const [email, setEmail] = useState("")
+   const [password, setPassword] = useState("")
+   
+   const { signIn } = useAuth()
+   
+   function handleSignIn() {
+      /* pegará o email e password digitado pelo usuário (com a ajuda do nosso useState) para ser 
+      usado na function signIn que está em nosso auth.jsx */
+      signIn({ email, password })
+   }
+   
    const navigate = useNavigate()
-
-   // exemplo
-   const { name, email } = useAuth()
-   console.log(name, email)
 
    return (
       <Container>
@@ -30,16 +37,19 @@ export function SignIn() {
             <span>Faça login</span>
             <InputLabel 
                htmlFor="1" id="1" title="Email" 
-               type="text" placeholder="Ex: maria@exemplo.com" 
+               type="text" placeholder="Ex: maria@exemplo.com"
+               onChange={e => setEmail(e.target.value)} 
             />
             
             <InputLabel 
                htmlFor="2" id="2" title="Senha" 
                type="password" placeholder="No mínimo 6 caracteres" 
+               onChange={e => setPassword(e.target.value)}
             />
 
             <Button 
                title="Entrar"
+               onClick={handleSignIn}
             />
 
             <a onClick={() => navigate("/register")}>Criar uma conta</a>
