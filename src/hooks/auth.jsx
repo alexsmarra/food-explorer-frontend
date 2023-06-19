6c7c58eb7207440218c23ea6fbf00aec54441e61
Alexandre Marra <alexsmarra@hotmail.com>
2023-06-19 08:53:03 -0300

    localStorage useAuth: setando o localStorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -5,6 +5,10 @@ import { api } from "../services/api.js"
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+   /* Quando demos um reload na página, esse estado será recarregado e ele reiniciará como um
+   estado vazio, por isso precisaremos armazenar os dados desse useState no localStorage, no
+   navegador, nele, mesmo com reload, permanecerá gravado. Dessa forma ao darmos um reload, 
+   a página não retornará para o auth.routes.js (em nosso caso) */
    const [data, setData] = useState({})
 
    /* params entre chaves, assim, sempre que utilizarmos a nossa function 'signIn' não precisaremos
@@ -16,6 +20,14 @@ function AuthProvider({ children }) {
          const response = await api.post("/sessions", { email, password })
 
          const { user, token } = response.data
+
+         /* Para armazenar informações no navegador do usuário. 
+         Precisaremos armazenar os dados do useState acima no localStorage (no navegador), nele, mesmo com reload, permanecerá gravado. Dessa forma ao darmos um reload, a página não retornará para o auth.routes.js (em nosso caso). 
+         To store in localStorage, it needs to be in text format */
+         localStorage.setItem("@rocketfood:user", JSON.stringify(user))
+         // não precisa converter para texto, já que o token já é um texto
+         localStorage.setItem("@rocketfood:token", token)
+
          /* Inserindo um token do tipo Bearer, de autorização, no cabeçalho por padrão em todas as
          requisições que o usuário for fazer a partir de agora */
          api.defaults.headers.common['Authorization'] = `Bearer ${token}`
