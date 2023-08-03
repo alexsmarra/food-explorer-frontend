ee35fc9f4cbc5aa84489e241708fe41204409e9f
Alexandre Marra <alexsmarra@hotmail.com>
2023-08-03 16:32:53 -0300

    teste, editando coluna name da tabela, alinhado com o commit a5231c8 do backend

--- src/pages/EditDish/index.jsx
+++ src/pages/EditDish/index.jsx
@@ -25,7 +25,7 @@ export const EditDish = () => {
    const [dish, setDish] = useState([])
 
    const [image, setImage] = useState(null)
-   const [name, setName] = useState("")
+   // const [name, setName] = useState("")
    const categoryParams = params.category
    const [selectedCategory, setSelectedCategory] = useState(categoryParams)
    // novo estado para armazenar os ingredientes 
@@ -73,26 +73,28 @@ export const EditDish = () => {
       setPrice(newPrice)
    }   
 
-   async function handleNewDish() {
+   console.log(params.id)
+
+   async function handleEditDish() {
       
       const formData = new FormData()
 
-      const fieldsToCheck = [
-         { name: 'image', value: image},
-         { name: 'name', value: name },
-         { name: 'category', value: selectedCategory},
-         // { name: "ingredients", value: ingredientList}
-         { name: 'price', value: price},
-         { name: 'description', value: description}
-      ]
+      // const fieldsToCheck = [
+      //    // { name: 'image', value: image},
+      //    { name: 'name', value: name },
+      //    // { name: 'category', value: selectedCategory},
+      //    // { name: "ingredients", value: ingredientList}
+      //    // { name: 'price', value: price},
+      //    // { name: 'description', value: description}
+      // ]
 
-      fieldsToCheck.forEach(field => {
-         if(field.value.length == 0) {
-            formData.append(field.name, dish[field.name])
-         } else {
-            formData.append(field.name, field.value)
-         }
-      })
+      // fieldsToCheck.forEach(field => {
+      //    if(field.value.length == 0) {
+      //       formData.append(field.name, dish[field.name])
+      //    } else {
+      //       formData.append(field.name, field.value)
+      //    }
+      // })
 
       /* A ideia do array e forEach acima é para fazer o que está abaixo com cada um */
       // if(name.length == 0) {
@@ -101,12 +103,14 @@ export const EditDish = () => {
       //    formData.append('name', name)
       // }
 
+      const name = "batata"
+      formData.append('name', name)
+
       try { 
          await api.patch(`/dishes/${params.id}`, formData)
             toast.success("Name atualizado com sucesso!", {
                autoClose: 1000
             })
-            // navigate(address)
       } catch( error) {
          if(error.response) {
             toast.error(error.response.data.message, {autoClose: 1000})
@@ -220,7 +224,7 @@ export const EditDish = () => {
                <Button
                   className="btn-form" 
                   title="Salvar alterações"
-                  onClick={handleNewDish}
+                  onClick={handleEditDish}
                />
          </div>
       </Form>
